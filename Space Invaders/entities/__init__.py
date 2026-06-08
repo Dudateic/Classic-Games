287b74826bcfd6c339a5910e215e12e2cdc009ab
duda <Dudateic>
2026-06-08 20:16:04 -0300

    feat: update snake and space invaders

---
+++ Space Invaders/entities/__init__.py
@@ -0,0 +1,5 @@
+from .player import Player
+from .spell import Spell
+from .enemy_shot import EnemyShot
+from .enemy import Enemy
+from .powerup import PowerUp
---
+++ Space Invaders/ui/__init__.py
@@ -0,0 +1,12 @@
+from ui.hud import HUD
+
+from ui.screens import (
+    MenuScreen,
+    draw_name_input,
+    draw_start_screen,
+    draw_ranking_screen,
+    draw_game_over,
+)
+
+from ui.starfield import make_starfield
+from ui.menu_ship import make_menu_ships
